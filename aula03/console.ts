58c72673649d341a9ba78230a8f66bc3386b3d62
Yasmim <martins.k.nappi@gmail.com>
2025-04-11 18:15:13 -0300

    Diferenças de Typescript para Javascript.

---
+++ aula03/console.ts
@@ -0,0 +1,11 @@
+function somar (a,b){
+    return a + b;
+}
+    console.log(somar(5,"10"));
+
+function SomarComTipo (a: number, b: number): number {
+    return a + b;
+}
+    console.log(SomarComTipo (5, 10));
+    SomarComTipo("oi", "tudo bom?")
+    SomarComTipo("oi", "tudo bem?");
